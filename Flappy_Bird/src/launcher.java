6945b8c3333d96b85d8fe5d2fb0f78257ca952ed
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2023-04-21 07:56:36 +0800

    Add files via upload

---
+++ Flappy_Bird/src/launcher.java
@@ -0,0 +1,9 @@
+public class launcher {
+	public static FlappyBird flappyBird;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		flappyBird = new FlappyBird();
+	}
+
+}
